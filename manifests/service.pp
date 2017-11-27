84041e316f2607d42ba9a4130ffc32cb46834863
root <root@centserver.mytest.com>
2017-11-26 21:06:52 -0500

    updates to files

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,6 +1,6 @@
 class ntp::service(
   String $service_name  	= $ntp::service_name,
-  String $service_ensure	= $ntp::servie_ensure,
+  String $service_ensure	= $ntp::service_ensure,
   Boolean $service_enable	= $ntp::service_enable,
   Boolean $servive_hasstatus    = $ntp::service_hasstatus,
   Boolean $service_hasrestart   = $ntp::service_hasrestart,
@@ -10,6 +10,6 @@ class ntp::service(
 	enable	=> $service_enable,
 	name	=> $service_name,
 	hasstatus => $service_hasstatus,
-	hasrestart => $service3_hasrestart,
+	hasrestart => $service_hasrestart,
   }
 }
